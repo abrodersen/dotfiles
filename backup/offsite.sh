4e45e270ab1b6a1c42e4214dd1bbf347542df0c2
Aaron Brodersen <aaron@abrodersen.com>
2025-02-27 14:12:26 -0600

    manage blesh and atuin

--- backup/offsite.sh
+++ backup/offsite.sh
@@ -19,6 +19,72 @@ _fatal() {
     exit 1
 }
 
+_readme() {
+    cat <<"EOF"
+# Backup Repository
+
+This is a repository for offsite backups containing data of high importance and
+sentimentality to the Brodersen household. This document contains instructions
+for reading and restoring data from the archives in the event that the
+original data or the cloud archives are unavailable.
+
+## Organization
+
+The drive contains a `backups` folder, which contains indiviual repositories of
+data. These repositories are formatted with [Restic](https://restic.net/), an
+open source backup software. The repositories contain application specific
+data. More details can be learned by restoring and inspecting individual files
+from the repositories.
+
+The repositories store the file data as snapshots. Each snapshot represents the
+state of all the files at a specific point in time. The Restic command line
+interface allows listing the snapshots in a repository and seeing the relevant
+timestamp. It also allows restoring files from a snapshot to another folder.
+
+## Password
+
+The repositories on this drive are encrypted. The password is stored on the
+root of this external drive in a file called `password.txt`. It can be
+referenced directly during restore operations like in the below examples.
+
+## Instructions
+
+To restore the data from a snapshot, follow these instructions specified below.
+If you need assistance, find a technical person with Linux experience to assist
+you with this process. The process is easiest on a Linux or macOS based system.
+
+1. On a working computer, follow the [Restic installation instructions](https://restic.readthedocs.io/en/stable/020_installation.html)
+to install the command line interface.
+
+2. Connect the external drive to a computer with Restic installed. Open a
+terminal window, and navigate to the root of the external drive.
+
+3. Run the following command to list the available snapshots in the repository.
+Be sure to replace `repository_name` with the name of the repository you wish
+to restore from.
+
+```bash
+restic -r /path/to/external/drive/backups/repository_name --password-file /path/to/external/drive/password.txt snapshots
+```
+
+The output of the command will look like this:
+```bash
+ID        Date                 Host        Tags        Directory
+----------------------------------------------------------------------
+d789xyz    2023-06-15 14:30    laptop      docs        /home/user/documents
+ab123de    2023-06-14 10:15    laptop      photos      /home/user/pictures
+f456uvw    2023-06-13 09:00    laptop      backup      /home/user/backup
+```
+
+4. Identify the snapshot you wish to restore files from. Run the following
+command to restore all files in the snapshot to the new location.
+
+```bash
+restic -r /path/to/external/drive/backups/repository_name --password-file /path/to/external/drive/password.txt restore d789xyz --target /path/to/new/location
+```
+EOF
+}
+
 _require_secret() {
     local value="$(secret-tool lookup $1 $2)"
     if [ -z "$value" ]; then
@@ -48,6 +114,15 @@ for label in "${labels[@]}"; do
         continue
     fi
 
+    instructions_file=$(mktemp)
+    _readme > "$instructions_file"
+    rsync -av --checksum "$instructions_file" "$disk_mount/instructions.txt"
+    password_file=$(mktemp)
+    target_restic_password="$(_require_secret restic_id offsite_password)"
+    printf "%s" "$target_restic_password" > "$password_file"
+    rsync -av --checksum "$password_file" "$disk_mount/password.txt"
+    echo "Backup procedure was last performed at $(date)." > "$disk_mount/timestamp.txt"
+
     _log "saving snapshots to disk mounted at $disk_mount ..."
     for repo in "${!repos[@]}"; do
         _log "processing repo $repo ..."
@@ -59,7 +134,6 @@ for label in "${labels[@]}"; do
 
         backblaze_secret_key="$(_require_secret key_id $backblaze_key_id)"
         source_restic_password="$(_require_secret restic_id $restic_lookup_id)"
-        target_restic_password="$(_require_secret restic_id offsite_password)"
 
         if [ ! -d "$output_repo" ]; then
             _log "directory $output_repo does not exist. initializing restic repo ..."
---
+++ blesh/init.sh
@@ -0,0 +1,4 @@
+bleopt complete_auto_complete=
+bleopt complete_auto_history=
+bleopt complete_ambiguous=
+bleopt prompt_eol_mark=''
